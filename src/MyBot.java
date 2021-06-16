54b096435da996a124e3eeecbfa743d8976f59a6
philippreinig <philipp.reinig@st.ovgu.de>
2021-06-16 15:53:56 +0200

    16.06 15:53

--- src/MyBot.java
+++ src/MyBot.java
@@ -8,6 +8,8 @@ import com.apogames.kitchenchef.ai.player.Player;
 import com.apogames.kitchenchef.game.entity.Vector;
 import com.apogames.kitchenchef.game.enums.CookingStatus;
 import com.apogames.kitchenchef.game.enums.KitchenDish;
+import com.apogames.kitchenchef.game.enums.KitchenIngredient;
+import com.apogames.kitchenchef.game.enums.KitchenSpice;
 import com.apogames.kitchenchef.game.pathfinding.PathResult;
 
 import java.util.ArrayList;
@@ -18,6 +20,15 @@ import java.util.List;
 @Student(author = "Philipp Reinig", matrikelnummer = 232934)
 public class MyBot extends KitchenPlayerAI {
     private static final Vector IDLE = Vector.ZERO;
+    public static final String COOKING = "COOKING";
+    public static final String CUTTING = "CUTTING";
+    public static final String INGREDIENT_TAKE = "INGREDIENT_TAKE";
+    public static final String CUSTOMER = "CUSTOMER";
+    public static final String UPGRADE = "UPGRADE";
+    public static final String SPICE_TAKE = "SPICE_TAKE";
+    public static final String BUY = "BUY";
+    public static final String DISH_WASHING = "DISH_WASHING";
+    public static final String DISH_TAKING = "DISH_TAKING";
     HashMap<String, ActionPoint> actionPoints;
     List<ActionPoint> customers;
     List<ActionPoint> cuttingActionPoints;
@@ -36,17 +47,11 @@ public class MyBot extends KitchenPlayerAI {
         this.ingredientTakingActionPoints = new ArrayList<>();
         this.cookingOnStove = false;
         this.currentCooking = null;
-
     }
 
     @Override
     public String getName() {
         return "OwnBot";
-    }
-
-    private Cooking getCookingFromID(final long id) {
-        for (final Cooking cooking : this.info.getCookings()) if (cooking.getId() == id) return cooking;
-        throw new IllegalStateException("No cooking in cookings of KitchenInformation with id: " + id);
     }
 
     @Override
@@ -71,14 +76,15 @@ public class MyBot extends KitchenPlayerAI {
                 else
                     this.moveToActionPointAndUse(player, this.actionPoints.get("DISH_WASHING"));
                 System.err.println("need to clean dish now");
-            } else if (status == CookingStatus.DISH)
-                this.moveToActionPointAndUse(player, this.ingredientTakingActionPoints.get(0));
-            else if (status == CookingStatus.RAW) {
-                if (!MyBot.allCorrect(this.currentCooking.getIngredientsCorrect()))
-                    this.moveToActionPointAndUse(player, this.ingredientTakingActionPoints.get(0));
-                else if (!MyBot.allCorrect(this.currentCooking.getSpiceCorrect()))
-                    this.moveToActionPointAndUse(player, this.actionPoints.get("SPICE_TAKE"));
-            } else if (status == CookingStatus.READY_FOR_CUTTING) {
+            } else if (status == CookingStatus.DISH || status == CookingStatus.RAW)
+                if (this.allIngredientsAndSpicesAvailable(player.getCooking()))
+                    if (!MyBot.allCorrect(this.currentCooking.getIngredientsCorrect()))
+                        this.moveToActionPointAndUse(player, this.ingredientTakingActionPoints.get(0));
+                    else if (!MyBot.allCorrect(this.currentCooking.getSpiceCorrect()))
+                        this.moveToActionPointAndUse(player, this.actionPoints.get("SPICE_TAKE"));
+                    else throw new IllegalStateException();
+                else this.moveToActionPointAndUse(player, this.actionPoints.get(MyBot.BUY));
+            else if (status == CookingStatus.READY_FOR_CUTTING) {
                 this.moveToActionPointAndUse(player, this.cuttingActionPoints.get(0));
                 if (player.getCooking() != null) System.out.println("updated cooking after cutting");
             } else if (status == CookingStatus.READY_FOR_COOKING) {
@@ -112,23 +118,23 @@ public class MyBot extends KitchenPlayerAI {
         for (final ActionPoint actionpoint : this.info.getActionPoints()) {
             final String name = actionpoint.getContent().name();
             switch (name) {
-                case "DISH_TAKING":
-                case "DISH_WASHING":
-                case "SPICE_TAKE":
-                case "BUY":
-                case "UPGRADE":
+                case MyBot.DISH_TAKING:
+                case MyBot.DISH_WASHING:
+                case MyBot.SPICE_TAKE:
+                case MyBot.BUY:
+                case MyBot.UPGRADE:
                     this.actionPoints.put(name, actionpoint);
                     break;
-                case "CUSTOMER":
+                case MyBot.CUSTOMER:
                     this.customers.add(actionpoint);
                     break;
-                case "INGREDIENT_TAKE":
+                case MyBot.INGREDIENT_TAKE:
                     this.ingredientTakingActionPoints.add(actionpoint);
                     break;
-                case "CUTTING":
+                case MyBot.CUTTING:
                     this.cuttingActionPoints.add(actionpoint);
                     break;
-                case "COOKING":
+                case MyBot.COOKING:
                     this.cookingActionPoints.add(actionpoint);
                     break;
                 default:
@@ -176,6 +182,42 @@ public class MyBot extends KitchenPlayerAI {
         for (final KitchenDish cleanKitchenDish : this.actionPoints.get("DISH_TAKING").getDishes())
             if (kitchenDishNeeded == cleanKitchenDish) return true;
         return false;
+    }
+
+    private static HashMap<KitchenIngredient, Integer> listOfIngredientsToHashMap(final List<KitchenIngredient> list) {
+        final HashMap<KitchenIngredient, Integer> ingredientsHashMap = new HashMap<>();
+        for (final KitchenIngredient element : list)
+            if (ingredientsHashMap.containsKey(element))
+                ingredientsHashMap.put(element, ingredientsHashMap.get(element) + 1);
+            else ingredientsHashMap.put(element, 0);
+        return ingredientsHashMap;
+    }
+
+    private static HashMap<KitchenSpice, Integer> listOfSpicesToHashMap(final List<KitchenSpice> list) {
+        final HashMap<KitchenSpice, Integer> spicesHashMap = new HashMap<>();
+        for (final KitchenSpice element : list)
+            if (spicesHashMap.containsKey(element))
+                spicesHashMap.put(element, spicesHashMap.get(element) + 1);
+            else spicesHashMap.put(element, 0);
+        return spicesHashMap;
+    }
+
+    private boolean allIngredientsAndSpicesAvailable(final Cooking cooking) {
+        final HashMap<KitchenIngredient, Integer> neededIngredients = MyBot.listOfIngredientsToHashMap(cooking.getRecipe().getNeededIngredients());
+        final HashMap<KitchenSpice, Integer> neededSpices = MyBot.listOfSpicesToHashMap(cooking.getRecipe().getNeededSpice());
+        final HashMap<KitchenIngredient, Integer> availableIngredients = MyBot.listOfIngredientsToHashMap(this.ingredientTakingActionPoints.get(0).getIngredients());
+        final HashMap<KitchenSpice, Integer> availableSpices = MyBot.listOfSpicesToHashMap(this.actionPoints.get(MyBot.SPICE_TAKE).getSpices());
+
+        for (final KitchenIngredient element : neededIngredients.keySet())
+            if (availableIngredients.get(element) == null || neededIngredients.get(element) > availableIngredients.get(element))
+                return false;
+
+        for (final KitchenSpice element : neededSpices.keySet())
+            if (availableSpices.get(element) == null || neededSpices.get(element) > availableSpices.get(element))
+                return false;
+
+        return true;
+
     }
 
     private ActionPoint getNextCustomer() {
